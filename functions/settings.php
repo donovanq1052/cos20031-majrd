14db7118dc36ca42eee03e5cae6ceec7d66a6ac5
reeve <102573274@student.swin.edu.au>
2025-06-01 22:20:52 +1000

    some temporary thing

--- functions/settings.php
+++ functions/settings.php
@@ -1,17 +1,17 @@
 <?php
-
+	/*
 	$host = "feenix-mariadb.swin.edu.au";
 	$user = "s103597864";
 	$pswd = "MSql*Int!9";
-	
+	*/
 	
 	// localhost db settings for laragon
 	
-	/*
+
 	$host = "127.0.0.1:3306";
 	$user = "root";
 	$pswd = "";
-	*/
+
 	
 
 	$dbnm = "s103597864_db";
