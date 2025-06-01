92d3e6884c6f190e0a6719aa8cbe87198bcd92d6
Matthew <128595283+matthxu@users.noreply.github.com>
2025-06-01 14:00:43 +1000

    Added css and view scores functionality

--- roundadd.php
+++ roundadd.php
@@ -14,37 +14,37 @@ session_start();
     <title>Add Round | MAJRD Archery</title>
 </head>
 <body>
-    
-<?php
-    include_once 'header.inc';
-    if(isset($_SESSION['roundmsg']))
-    {
-        echo $_SESSION['roundmsg'];
-        unset($_SESSION['roundmsg']);
-    }
-    if(isset($_SESSION['added']))
-    {
-        echo $_SESSION['added'];
-        unset($_SESSION['added']);
-    }
-?>
+    <?php
+        include_once 'header.inc';
+        if(isset($_SESSION['roundmsg']))
+        {
+            echo $_SESSION['roundmsg'];
+            unset($_SESSION['roundmsg']);
+        }
+        if(isset($_SESSION['added']))
+        {
+            echo $_SESSION['added'];
+            unset($_SESSION['added']);
+        }
+    ?>
+    <main>
+        <h2>Add a round</h2>
+        <form method="post" action="rangesadd.php">
 
-<h2>Add a round</h2>
-<form method="post" action="rangesadd.php">
+            <label for="roundname">Enter round name. Round name must be unique</label>
+            <input type="text" name="roundname" id="roundname" placeholder="Enter Round Name">
+            <br><br>
+            <label for="ranges">How many ranges will be in this round?</label>
+            <input type="number" name="ranges" id="ranges" min="1" max="7">
+            <br><br>
+            <button type="submit" name="action">Press here to input the ranges</button>
+            <br><br>
 
-    <label for="roundname">Enter round name. Round name must be unique</label>
-    <input type="text" name="roundname" id="roundname" placeholder="Enter Round Name">
-    <br><br>
-    <label for="ranges">How many ranges will be in this round?</label>
-    <input type="number" name="ranges" id="ranges" min="1" max="7">
-    <br><br>
-    <button type="submit" name="action">Press here to input the ranges</button>
-    <br><br>
+        </form>
 
-</form>
-
-<?php
-    include_once 'footer.inc';
-?>
+        <?php
+            include_once 'footer.inc';
+        ?>
+    </main>
 </body>
 </html>
